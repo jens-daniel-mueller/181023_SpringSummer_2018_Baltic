af46d480a588c6d5cb8b893255f5bc6c07546165
jens-daniel-mueller <jens.mueller@io-warnemuende.de>
2018-12-05 15:47:22 +0100

    stack read-in of matrix data files
    gridding to coordinate raster
    
    monthly mean calculation

--- Read_Remote_Sensing_data_files.R
+++ Read_Remote_Sensing_data_files.R
@@ -4,81 +4,134 @@ library(lubridate)
 library(tidyverse)
 
 
-### matrix data
+### read coordinate grid data
 
-setwd("C:/Mueller_Jens_Data/181023_Spring_Summer_2018/data/Remote_Sensing/Woz_Chl_20180530")
+setwd("C:/Mueller_Jens_Data/181023_Spring_Summer_2018/data/Remote_Sensing/grid")
 files <- list.files(pattern = "[.]txt$")
 
 lat <- read.delim(files[1], header = FALSE)
-lat.long <-
+lat <-
   lat %>% 
   gather(V1:V1000) %>% 
   select(value) %>% 
   rename(lat = value)
-rm(lat)
 
 lon <- read.delim(files[2], header = FALSE)
-lon.long <-
+lon <-
   lon %>% 
   gather(V1:V1000) %>% 
   select(value) %>% 
   rename(lon = value)
-rm(lon)
 
-chl <- read.delim(files[3], header = FALSE)
-chl.long <-
+rm(files)
+
+
+
+
+### read chl matrix data and merge with coordinate grid
+
+setwd("C:/Mueller_Jens_Data/181023_Spring_Summer_2018/data/Remote_Sensing/Chl")
+files <- list.files(pattern = "[.]txt$")
+#file <- files[1]
+
+for (file in files){
+
+chl <- read.delim(file, header = FALSE)
+chl <-
   chl %>% 
   gather(V1:V1000) %>% 
-  select(value) %>% 
-  rename(chl = value)
-rm(chl)
+  select(value)
 
 df <- data.table(
-  cbind(lat.long, lon.long, chl.long))
+  cbind(lat, lon, chl))
 
-rm(lat.long, lon.long, chl.long)
+rm(chl)
 
-summary(df)
+df <- na.omit(df)
 
-df$lat.int <- as.numeric( as.character (cut(df$lat, seq(50.975, 72.025, 0.05), labels = seq(51, 72, 0.05))))
-df$lon.int <- as.numeric( as.character (cut(df$lon, seq(8.975, 32.025, 0.05), labels = seq(9, 32, 0.05))))
+df$lat.int <- as.numeric( as.character (cut(df$lat, seq(50.75, 72.25, 0.5), labels = seq(51, 72, 0.5))))
+df$lon.int <- as.numeric( as.character (cut(df$lon, seq(8.75, 32.25, 0.5), labels = seq(9, 32, 0.5))))
 
 matrix.int <-
-  data.table(
   df %>% 
   group_by(lat.int, lon.int) %>% 
-  summarise(mean.chl = mean(chl)) %>% 
-  ungroup()
-  )
+  summarise(chl = mean(value)) %>% 
+  ungroup() %>% 
+  add_column(year  = substr(file, 9, 12),
+             month = substr(file, 13, 14),
+             day = substr(file, 15, 16))
 
-#rm(df)
+rm(df)
+
+if (exists("temp")){
+  temp <- rbind (temp, matrix.int)
+} else{temp <- matrix.int}
+
+rm(matrix.int)
+
+print(file)
+
+}
+
+rm(file, files)
+
+df <- temp
+rm(temp)
 
 
-# ### long table data
-# 
-# setwd("C:/Mueller_Jens_Data/181023_Spring_Summer_2018/data/Remote_Sensing/testing_new_format")
-# df <- read.delim("pandas.txt", header = FALSE)
-# 
-# names(df) <- c("lat", "lon", "chl")
-# 
-# df$lat.int <- as.numeric( as.character (cut(df$lat, seq(50.95, 72.05, 0.1), labels = seq(51, 72, 0.1))))
-# df$lon.int <- as.numeric( as.character (cut(df$lon, seq(8.95, 32.05, 0.1), labels = seq(9, 32, 0.1))))
-# 
-# table.int <-
-#   df %>% 
-#   group_by(lat.int, lon.int) %>% 
-#   summarise(mean.chl = mean(chl)) %>% 
-#   ungroup()
-# 
-# rm(df)
+
 
 #plot results from both data sources
 
 
 setwd("C:/Mueller_Jens_Data/181023_Spring_Summer_2018/data/Remote_Sensing/plots")
 
+df <- data.table(df)
+df$chl.int <- cut(df$chl, c(0,2,5,10,15,20,50))
 
-ggplot(matrix.int, aes(lon.int, lat.int, fill=mean.chl))+
+
+df.month <- 
+  df %>% 
+  group_by(lat.int, lon.int, year, month) %>% 
+  summarise(chl = mean(chl)) %>%
+  mutate(chl.int = cut(chl, c(0,2,4,6,10,12.5,15,20,50))) %>% 
+  ungroup()
+
+
+
+
+
+ggplot(df[year == "2016"], aes(lon.int, lat.int, fill=chl.int))+
+  geom_raster()+
+  scale_fill_viridis_d()+
+  coord_quickmap()+
+  facet_grid(day~month)
+ggsave("2016_Chl_data.pdf", height = 600, width = 200, dpi = 300, units = "mm")
+
+ggplot(df[year == "2018"], aes(lon.int, lat.int, fill=chl.int))+
+  geom_raster()+
+  scale_fill_viridis_d()+
+  coord_quickmap()+
+  facet_grid(day~month)
+ggsave("2018_Chl_data.pdf", height = 600, width = 200, dpi = 300, units = "mm")
+
+
+ggplot(df.month, aes(lon.int, lat.int, fill=chl.int))+
+  geom_raster()+
+  scale_fill_brewer(palette = "Spectral", direction = -1)+
+  coord_quickmap()+
+  facet_grid(year~month)
+ggsave("monthly_Chl_data.pdf", height = 100, width = 200, dpi = 300, units = "mm")
+
+
+
+
+
+
+
+
+
+ggplot(matrix.int, aes(lon.int, lat.int, fill=chl))+
   geom_raster()+
   scale_fill_viridis_c(limits = c(0,10))+
   coord_quickmap()
